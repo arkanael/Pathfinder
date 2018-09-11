b9f9373f1fcd742a27f3c1fb13926cc496e03cab
Luiz Guilherme Bandeira <arkanael@gmail.com>
2018-09-11 13:34:59 -0300

    Feito: CategoryController

---
+++ Pathfinder/Controllers/CategoryController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pathfinder.Data.Context;
+using Pathfinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pathfinder.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public CategoryController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [Route("v1/categories")]
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return _dataContext.Categories.AsNoTracking().ToList();
+        }
+
+        [Route("v1/categories/{id}")]
+        [HttpGet]
+        public Category Get(int id)
+        {
+            return _dataContext.Categories.AsNoTracking().Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        [Route("v1/categories/{id}/produtcs")]
+        [HttpGet]
+        public IEnumerable<Product> GetProducts(int id)
+        {
+            return _dataContext.Products.AsNoTracking().Where(c => c.CategoryId == id).ToList();
+        }
+
+        [Route("v1/categories")]
+        [HttpPost]
+        public Category Post([FromBody] Category category)
+        {
+            _dataContext.Categories.Add(category);
+            _dataContext.SaveChanges();
+
+            return category;
+        }
+
+        [Route("v1/categories")]
+        [HttpPost]
+        public Category Put([FromBody] Category category)
+        {
+            _dataContext.Entry<Category>(category).State = EntityState.Modified;
+            _dataContext.SaveChanges();
+
+            return category;
+        }
+
+        [Route("v1/categories")]
+        [HttpPost]
+        public Category Delete([FromBody] Category category)
+        {
+            _dataContext.Categories.Remove(category);
+            _dataContext.SaveChanges();
+
+            return category;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
+Projeto MARS Pathfinder. Controle de Produtos.
 WebApi ASP.NET CORE
+.NET CORE
+Failfast validation
+MVC
+Data Driven Design
+Repository Pattern
+Entity Framework Core
+Swagger
+Queries
+Versionamento
+Cache
+Compressão
+Monitoramento
+SQL SERVER
+SQL Operations Studio
+DOCKER
+
+
+
+
+
+
+
+
+
+
+Sobre:
+Mars Pathfinder foi uma missão espacial norte-americana lançada em meados de 1996 que tinha como objetivo 
+principal enviar um robô para a superfície de Marte a fim de estudar melhor o planeta.
+
+A Pathfinder (nave-mãe e módulo de pouso) usou um método inovador para entrar diretamente na atmosfera de Marte 
+auxiliado por um pára-quedas supersônico, que reduziu sua velocidade de descida, e um conjunto de 24 airbags laterais para 
+amaciar o impacto com o solo.
+
+O pouso foi em 4 de julho de 1997 na planície de Ares Vallis, no hemisfério norte de Marte. 
+O local exato do pouso foi batizado de "Memorial Carl Sagan", em homenagem ao grande cientista e divulgador Carl Sagan (1934 - 1996).
+
+O robô explorador Sojourner passeou pela superfície de Marte recolhendo informações durante mais de um mês terrestre, 
+no total foram obtidas 16.500 fotos a partir do módulo de pouso e 550 imagens do Sojourner. 
